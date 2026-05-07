860253402e9a1b1cb67fce8c2796e536b64e1bbd
GabrielNepomuceno1101 <gahnepomuceno1101@icloud.com>
2026-05-07 20:49:18 -0300

    Aula em java
    Colocando em dia

---
+++ main.java
@@ -0,0 +1,47 @@
+// Poo -> Promoção Orientada Objeto.
+// É uma linguagem Fortemente tipada.
+// indepedente de plataforma(Multiplataforma).
+// JVM -> Java Vitual Machine.
+
+// A JVM é responsavel por realizar a interpretação
+// do bytecode e pela execução.
+
+// Uma variavel é um espaço na memoria nde podemos 
+// armazenar valores
+
+//Tipos primitivos 
+//byte 8bites-> 128 a 127
+//short 16bits-> -32.768 a 32.767
+//int 32bits -> 2.127.483.648 a 2.147.483.647
+//long 64bits -> - 9.223.372.036.854.775.808 a 9.223.372.036.854.775.808
+public clas main {
+
+public static void main(Strin[] args) {
+
+    int minhaIdade = 16;
+   String teste = "Alo mundo";
+   Var testando = "Gabriel"
+   Var teste2 = 52;
+   byte n1 = -127;
+   testando = "Fernada";
+//Declarar uma variavel -> tipo Variavel = Valor
+//Valor da variavel usando o var ele
+// ira inferir o tipo automaticamente.
+
+
+
+
+System.out.println(teste);
+System.out.println(minhaIdade);
+
+
+}
+
+
+
+}
+
+class carro{
+
+
+}
