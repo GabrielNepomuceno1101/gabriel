ec5a968f2edc04d3e9ee7cdd4e145243cc6c393c
GabrielNepomuceno1101 <gahnepomuceno1101@icloud.com>
2026-05-20 21:17:49 -0300

    Vetor
    Primeira aula de Vetor

--- main.java
+++ main.java
@@ -14,14 +14,14 @@
 //short 16bits-> -32.768 a 32.767
 //int 32bits -> 2.127.483.648 a 2.147.483.647
 //long 64bits -> - 9.223.372.036.854.775.808 a 9.223.372.036.854.775.808
-public clas main {
+public class main {
 
-public static void main(Strin[] args) {
+public static void main(String[] args) {
 
     int minhaIdade = 16;
    String teste = "Alo mundo";
-   Var testando = "Gabriel"
-   Var teste2 = 52;
+   var testando = "Gabriel";
+   var teste2 = 52;
    byte n1 = -127;
    testando = "Fernada";
 //Declarar uma variavel -> tipo Variavel = Valor
@@ -41,7 +41,26 @@ System.out.println(minhaIdade);
 
 }
 
-class carro{
+class aula01{
+    public static void main(String[] args){
+        byte b = 100;
+        short s = 10000;
+        int i[] ={1,2,3,4,5} ;
+        //posição 0 1 2 3 4
+        long l = 1000000;
+        float f = 10.5f;
+        double d = 20.5;
+        char c = 'A';
+        boolean bool = false;
+        String nome[] ={"Gabriel","João","William","Erik"};
+        // posição         0         1       2        3
+        if (bool){
+            System.out.println("VERDADEIRO");
 
+        }else{
+            System.out.println("Errado o nome é: "+nome[0] +" "+nome[1] +" "+nome[2]);
+
+        }
+    }
 
 }
